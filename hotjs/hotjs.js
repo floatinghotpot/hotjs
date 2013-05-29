46e5766485b0a1b8c87d9ab26fe2745f7c60bf9b
Liming Xie <liming.xie@gmail.com>
2013-05-29 15:52:39 +0800

    restructure folders

---
+++ hotjs/hotjs.js
@@ -0,0 +1,23 @@
+
+// name space
+var hotjs = hotjs || {};
+
+// app, render, view -> layer -> entity
+
+// event: mouse, touch, keyboard, userdefined
+
+// resource: audio, image, sprite, video
+
+// animation: move, rotate, scale, fade
+
+// math: point, vector, matrix, 
+
+// physics: collision, gravity, momentum, 
+
+// tool: UT, benchmark, debug, profiling,
+
+// AI: pathfinding
+
+// addon:
+
+// 
