2424cd42e6cab405e6b727e2c2c1d365efd2de35
Liming Xie <liming.xie@gmail.com>
2013-07-02 18:09:22 +0800

    working on i18n, not tested yet

---
+++ hotjs/demos/lang/en.lang.js
@@ -0,0 +1,7 @@
+
+lang_cache = lang_cache || {};
+lang_cache['en'] = lang_cache['en'] || {};
+lang_cache['en'] = $.extend( true, lang_cache['en'], {
+	
+	
+});
---
+++ hotjs/demos/lang/zh.lang.js
@@ -0,0 +1,7 @@
+
+lang_cache = lang_cache || {};
+lang_cache['zh'] = lang_cache['zh'] || {};
+lang_cache['zh'] = $.extend( true, lang_cache['zh'], {
+	
+	
+});
