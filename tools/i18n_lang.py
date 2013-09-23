9bd825dfc46d54dea576dc6b588a959604b8de72
Liming Xie <liming.xie@gmail.com>
2013-09-24 00:18:50 +0800

    fnished tool i18n_lang.py, to extract words in i18n=* or _T(*) to js file for localization

--- tools/i18n_lang.py
+++ tools/i18n_lang.py
@@ -33,10 +33,27 @@ curdir = os.path.abspath('.')
 srcdir = ''
 langdir = ''
 
-words = []
+words = {}
 dicts = { 'en':{}, 'zh':{}, 'ja':{} }
 
 def scan_words( path ):
+    if(not path.endswith('.js')): return
+    
+    fh = open( path, 'r' )
+    txt = fh.read()
+    fh.close()
+    
+    p = re.compile(r"_T\(\s*['\"](\w*)['\"]\s*\)")
+    w = p.findall(txt)
+    for k in w:
+        if( len(k) == 0 ): continue
+        words[ k ] = 1
+        
+    p = re.compile(r"\s*i18n=['\"](\w*)['\"]\s*")
+    w = p.findall(txt)
+    for k in w:
+        words[ k ] = 1
+
     pass
 
 def scan_words_recursive( path ):
@@ -51,11 +68,76 @@ def scan_words_recursive( path ):
     else:
         print( 'file not found: ' + path )
 
-def import_dict( f ):
+def import_dict( lang, f ):
+    dict = dicts[ lang ]
+    str1 = 'hotjs.i18n.put(\'' + lang + '\', {'
+    str2 = '});'
+    
+    fh = open( f, 'r' )
+    txt = fh.read()
+    fh.close()
+    
+    lines = txt.replace(str1, '').replace(str2, '').split('\n')
+    p = re.compile(r"^'(\w*)'\s*:\s*(.*)$")
+    dict = {}
+    for line in lines:
+        line = re.sub(r'(,$)', '', line.strip()).strip()
+        if( len(line) == 0 ): continue
+        
+        #print( line )
+        kv = p.findall( line )
+        k = kv[0][0]
+        v = kv[0][1]
+        v = re.sub(r"(^['\"])|(['\"]$)", '', v.strip())
+        #print( k + ': ' + v )
+        dict[ k ] = v
+        
+        if(lang == 'zh'): 
+            words[k] = 1
+        
+    #print( dict )
+    dicts[ lang ] = dict
     pass
 
-def export_dict( f ):
+def import_dict_files( dir ):
+    for k in dicts.keys():
+        f = dir + '/' + k + '.lang.js'
+        print( 'importing from: %s' % f )
+        import_dict( k, f )
     pass
+
+def export_dict( lang, f ):
+    dict = dicts[ lang ]
+    #print( dict )
+    
+    str1 = 'hotjs.i18n.put(\'' + lang + '\', {\n'
+    str2 = '\n});\n'
+    
+    fh = open( f, 'w' ) 
+    fh.write( str1 )
+
+    w = sorted( words.keys() )
+    #print( w )
+    #print( '\n' )
+    newdict = []    
+    for k in w:
+        if( len(k) == 0 ): continue
+        if( dict.has_key(k)):
+            v = dict[k]
+        else:
+            v = ''
+        newdict.append( "\t'" + k + "' : " + "'" + v + "'" )
+    
+    fh.write( ",\n".join(newdict) ) 
+    fh.write( str2 )
+    fh.close()  
+    pass
+
+def export_dict_files( dir ):
+    for k in dicts.keys():
+        f = dir + '/gen-' + k + '.lang.js'
+        print( 'exporting to: %s' % f )
+        export_dict( k, f )
 
 def main():
     """The entrypoint for this script."""
@@ -66,7 +148,6 @@ def main():
     parser = optparse.OptionParser(usage)
     parser.add_option("-i", "--input", dest="inputpath", help="input lang folder")
     parser.add_option("-o", "--output", dest="outputpath", help="output lang folder")
-    #parser.add_option("-s", "--scan", dest="scanpath", help="folder or file to scan words")
     
     (options, args) = parser.parse_args()
     
@@ -74,14 +155,21 @@ def main():
         parser.error('lacking arguments, please give source file path to scan.\n')
         exit(1)
     
-    import_dict( options.inputpath )
+    if( isinstance(options.inputpath, str) ):
+        import_dict_files( options.inputpath )
     
-    scan_words_
+    for f in args:
+        print( 'scanning: %s' % f )
+        scan_words( f )
     
-    for arg in args:
-        minify_recursive( arg, options )
+    print( 'Totally %d words to be localized.\n' % len(words) )
+    print( sorted(words.keys()) )
+    print( '' )
+
+    if( isinstance(options.outputpath, str) ):
+        export_dict_files( options.outputpath )
     
-    print( 'Done.\n')
+    print( '\nDone.\n')
 
 if __name__ == '__main__':
     main()
