2331cc2a51a590b3fd435762f549bf4f8c9f6931
Liming Xie <liming.xie@gmail.com>
2013-10-06 23:46:38 +0800

    bugfix for callAPI by func name, since minify will change func name

--- hotjs/lib/hotjs-bin.js
+++ hotjs/lib/hotjs-bin.js
@@ -3544,7 +3544,7 @@ AjaxClient.prototype = {
 		return false;
 	},
 	hello : function hello() {
-		var msg = this.callAPI( arguments.callee.name, { name: 'hotjs' } );
+		var msg = this.callAPI( 'hello', { name: 'hotjs' } );
 		return (!! msg) ? msg.name : false;
 	}
 };
@@ -3578,7 +3578,7 @@ hotjs.inherit( User, AjaxClient, {
 		this.hb_interval = hb;
 	},
 	registerAccount : function registerAccount(u, p, fn, e, c) {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'registerAccount', {
 			username : u,
 			password : p,
 			fullname : fn,
@@ -3589,7 +3589,7 @@ hotjs.inherit( User, AjaxClient, {
 		return (!! msg) ? msg.done : false;
 	},
 	deleteAccount : function deleteAccount() {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'deleteAccount', {
 			username : u,
 			password : p
 		} );
@@ -3597,7 +3597,7 @@ hotjs.inherit( User, AjaxClient, {
 	},
 	// return username & password in msg
 	autoRegister : function autoRegister(uuid) {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'autoRegister', {
 			uuid : uuid
 		});
 		
@@ -3605,7 +3605,7 @@ hotjs.inherit( User, AjaxClient, {
 	},
 	
 	login : function login(u, p, app, ver, hb) {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'login', {
 			username : u,
 			password : p,
 			app : app,
@@ -3636,7 +3636,7 @@ hotjs.inherit( User, AjaxClient, {
 	heartbeat : function heartbeat() {
 		if( !! this.session ) {
 			// send heartbeat msg
-			var api = arguments.callee.name;
+			var api = 'heartbeat';
 			var data = { sid:this.session, t:Date.now() };
 			this.postMsg( { 
 				api: api, 
@@ -3657,7 +3657,7 @@ hotjs.inherit( User, AjaxClient, {
 		}
 		
 		// send logout msg
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'logout', {
 			sid : this.session
 		});
 		this.session = "";
@@ -3671,13 +3671,13 @@ hotjs.inherit( User, AjaxClient, {
 				newusername : newu,
 				newpwd : newpwd
 			};
-		var msg = this.callAPI( arguments.callee.name, param);
+		var msg = this.callAPI( 'changeIdPassword', param);
 		return (!! msg) ? msg.done : false;
 	},
 	
 	// { public: {}, protected: {}, private: {} }
 	updateProfile : function updateProfile( data ) {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'updateProfile', {
 			sid : this.session,
 			profile : data
 		} );
@@ -3685,7 +3685,7 @@ hotjs.inherit( User, AjaxClient, {
 	},
 	// get own profile: {}
 	getProfile : function getProfile() {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'getProfile', {
 			sid : this.session
 		} );
 		return ((!! msg) && msg.done) ? msg.profile : false;
@@ -3694,7 +3694,7 @@ hotjs.inherit( User, AjaxClient, {
 	// return: msg.profile as a list: [ {}, {}, {} ]
 	// only return public part & protected on situation, never return private profile
 	queryProfile : function queryProfile( user ) {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'queryProfile', {
 			sid : this.session,
 			user : user
 		} );
@@ -3704,7 +3704,7 @@ hotjs.inherit( User, AjaxClient, {
 	// query profile of others, may be many [ user1, user2, user3, ... ]
 	// return: msg.user as a list: [ { username:"user1", fullname: "David", presence: "online" }, ... ]
 	listFriend : function listFriend() {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'listFriend', {
 			sid : this.session
 		} );
 		if ( (!! msg) && msg.done && Array.isArray(msg.user) ) {
@@ -3721,7 +3721,7 @@ hotjs.inherit( User, AjaxClient, {
 		if( this.friends.indexOf(username) > -1 ) {
 			return false;
 		}
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'addFriend', {
 			sid : this.session,
 			username : username,
 			textmsg : textmsg
@@ -3736,7 +3736,7 @@ hotjs.inherit( User, AjaxClient, {
 		if( this.friends.indexOf(username) > -1 ) {
 			return false;
 		}
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'confirmAddFriend', {
 			sid : this.session,
 			username : username
 		});
@@ -3750,7 +3750,7 @@ hotjs.inherit( User, AjaxClient, {
 		if( this.friends.indexOf(username) == -1 ) {
 			return false;
 		}
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'removeFriend', {
 			sid : this.session,
 			username : username
 		});
@@ -3767,7 +3767,7 @@ hotjs.inherit( User, AjaxClient, {
 	// query profile of others, may be many [ user1, user2, user3, ... ]
 	// return: msg.user as a list: [ { username:"user1", fullname: "David", presence: "online" }, ... ]
 	listBlock : function listBlock() {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'listBlock', {
 			sid : this.session
 		} );
 		if ( (!! msg) && msg.done && Array.isArray(msg.user) ) {
@@ -3785,7 +3785,7 @@ hotjs.inherit( User, AjaxClient, {
 		if( this.friends.indexOf(u) > -1 ) {
 			return false;
 		}
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'block', {
 			sid : this.session,
 			username : u
 		});
@@ -3799,7 +3799,7 @@ hotjs.inherit( User, AjaxClient, {
 		if( this.friends.indexOf(u) == -1 ) {
 			return false;
 		}
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'unblock', {
 			sid : this.session,
 			username : u
 		});
@@ -3815,7 +3815,7 @@ hotjs.inherit( User, AjaxClient, {
 
 	// set tags, [ "hacker", "swimming", "golf", ... ]
 	setTag : function addTag( data ) {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'setTag', {
 			sid : this.session,
 			tag : data
 		} );
@@ -3828,7 +3828,7 @@ hotjs.inherit( User, AjaxClient, {
 		return false;
 	},
 	getTag : function getTag() {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'getTag', {
 			sid : this.session
 		} );
 		if( (!! msg) && msg.done ) {
@@ -3842,7 +3842,7 @@ hotjs.inherit( User, AjaxClient, {
 	},
 	// { presence: "busy", location: [lat, lang] }
 	updateStatus : function updateStatus( data ) {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'updateStatus', {
 			sid : this.session,
 			status : data
 		});
@@ -3856,7 +3856,7 @@ hotjs.inherit( User, AjaxClient, {
 	// query presence of others, may be many [ user1, user2, user3, ... ]
 	// return [ "available"/"busy"/"nodistrub"/"rightback"/"away"/"offline" ]
 	queryPresence : function queryPresence( users ) {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'queryPresence', {
 			sid : this.session,
 			user : users
 		} );
@@ -3865,7 +3865,7 @@ hotjs.inherit( User, AjaxClient, {
 	
 	// return: msg.user as a list: [ { username:"user1", fullname: "David", presence: "online" }, ... ]
 	searchUser : function searchUser( tag, distance ) {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'searchUser', {
 			sid : this.session,
 			tag : tag,
 			distance : distance
@@ -3875,7 +3875,7 @@ hotjs.inherit( User, AjaxClient, {
 	// filter is string with complex condition, like: "(age > 25) AND (age < 40) AND (tag CONTAIN 'reading')" :-)
 	// return: msg.user as a list: [ { username:"user1", fullname: "David", presence: "online" }, ... ]
 	searchUserComplex : function searchUserComplex( filter ) {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'searchUserComplex', {
 			sid : this.session,
 			filter : filter
 		} );
@@ -3884,7 +3884,7 @@ hotjs.inherit( User, AjaxClient, {
 	
 	// invite a friend to join a group
 	inviteJoinGroup : function inviteJoinGroup( name, g ) {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'inviteJoinGroup', {
 			sid : this.session,
 			name : name,
 			group : g
@@ -3892,7 +3892,7 @@ hotjs.inherit( User, AjaxClient, {
 		return ((!! msg) && msg.done) ? true : false;
 	},
 	quitGroup : function quitGroup( g ) {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'quitGroup', {
 			sid : this.session,
 			group : g
 		} );
@@ -3900,7 +3900,7 @@ hotjs.inherit( User, AjaxClient, {
 	},
 	// chat in a group
 	chat : function chat( s, g ) {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'chat', {
 			sid : this.session,
 			group : g,
 			what : s
@@ -3910,7 +3910,7 @@ hotjs.inherit( User, AjaxClient, {
 	
 	// return msg.room [ "room1", "room2", ... ], not all, limit to 20
 	findRoom : function findRoom( name ) {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'findRoom', {
 			sid : this.session,
 			name : name
 		} );
@@ -3919,7 +3919,7 @@ hotjs.inherit( User, AjaxClient, {
 	// if room not exist, then create it.
 	// may need enter password, if required
 	enterRoom : function enterRoom( name, pwd ) {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'enterRoom', {
 			sid : this.session,
 			name : name,
 			secret : pwd
@@ -3928,7 +3928,7 @@ hotjs.inherit( User, AjaxClient, {
 	},
 	// if no one in room after exist, then remove the password if there is.
 	leaveRoom : function leaveRoom( name ) {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'leaveRoom', {
 			sid : this.session,
 			name : name
 		} );
@@ -3936,7 +3936,7 @@ hotjs.inherit( User, AjaxClient, {
 	},
 	// say something, anyone in the room can hear.
 	say : function say( s ) {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'say', {
 			sid : this.session,
 			what : s
 		} );
@@ -3945,7 +3945,7 @@ hotjs.inherit( User, AjaxClient, {
 	
 	// send short message, only between friends.
 	sms : function sms( name, s ) {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'sms', {
 			sid : this.session,
 			name : name,
 			what : s
@@ -3954,7 +3954,7 @@ hotjs.inherit( User, AjaxClient, {
 	},
 	
 	searchGamer : function searchGamer( appkey, level ) {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'searchGamer', {
 			sid : this.session,
 			appkey : appkey,
 			level : level
@@ -3964,7 +3964,7 @@ hotjs.inherit( User, AjaxClient, {
 	},
 	
 	uploadGameData : function uploadGameData( appkey, md5key, result, steps ) {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'uploadGameData', {
 			sid : this.session,
 			appkey : appkey,
 			md5key : md5key,
@@ -3977,7 +3977,7 @@ hotjs.inherit( User, AjaxClient, {
 	listGameData : function listGameData( appkey, pagesize, pageindex ) {
 		if( pageindex === undefined ) pageindex = 0;
 		if( pagesize === undefined ) pagesize = 10;
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'listGameData', {
 			sid : this.session,
 			appkey : appkey,
 			pagesize : pagesize,
@@ -3988,7 +3988,7 @@ hotjs.inherit( User, AjaxClient, {
 	},
 	// msg = { "md5key" : "xx", "result" : "xx", "steps" : "xx" }
 	downloadGameData : function downloadGameData( appkey, md5key ) {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'downloadGameData', {
 			sid : this.session,
 			appkey : appkey,
 			md5key : md5key
@@ -3997,7 +3997,7 @@ hotjs.inherit( User, AjaxClient, {
 		return msg;
 	},
 	voteGameData : function voteGameData( appkey, md5key, rating ) {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'voteGameData', {
 			sid : this.session,
 			appkey : appkey,
 			md5key : md5key,
@@ -4008,7 +4008,7 @@ hotjs.inherit( User, AjaxClient, {
 	},
 	
 	updateGameScore : function updateGameScore( appkey, data1, data2, data3 ) {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'updateGameScore', {
 			sid : this.session,
 			appkey : appkey,
 			data1 : data1,
@@ -4019,7 +4019,7 @@ hotjs.inherit( User, AjaxClient, {
 	},
 	// { "data1" : [ { "name" : "tom", "score" : 10 }, { ... } ], "data2" : [], "data3" : [] }
 	getGameScoreTop10 : function getGameScoreTop10( appkey ) {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'getGameScoreTop10', {
 			sid : this.session,
 			appkey : appkey
 		});
@@ -4027,7 +4027,7 @@ hotjs.inherit( User, AjaxClient, {
 	},
 	
 	feedback : function feedback( appkey, txt ) {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'feedback', {
 			sid : this.session,
 			msg : txt
 		});
--- hotjs/social.js
+++ hotjs/social.js
@@ -193,7 +193,7 @@ AjaxClient.prototype = {
 		return false;
 	},
 	hello : function hello() {
-		var msg = this.callAPI( arguments.callee.name, { name: 'hotjs' } );
+		var msg = this.callAPI( 'hello', { name: 'hotjs' } );
 		return (!! msg) ? msg.name : false;
 	}
 };
@@ -227,7 +227,7 @@ hotjs.inherit( User, AjaxClient, {
 		this.hb_interval = hb;
 	},
 	registerAccount : function registerAccount(u, p, fn, e, c) {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'registerAccount', {
 			username : u,
 			password : p,
 			fullname : fn,
@@ -238,7 +238,7 @@ hotjs.inherit( User, AjaxClient, {
 		return (!! msg) ? msg.done : false;
 	},
 	deleteAccount : function deleteAccount() {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'deleteAccount', {
 			username : u,
 			password : p
 		} );
@@ -246,7 +246,7 @@ hotjs.inherit( User, AjaxClient, {
 	},
 	// return username & password in msg
 	autoRegister : function autoRegister(uuid) {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'autoRegister', {
 			uuid : uuid
 		});
 		
@@ -254,7 +254,7 @@ hotjs.inherit( User, AjaxClient, {
 	},
 	
 	login : function login(u, p, app, ver, hb) {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'login', {
 			username : u,
 			password : p,
 			app : app,
@@ -285,7 +285,7 @@ hotjs.inherit( User, AjaxClient, {
 	heartbeat : function heartbeat() {
 		if( !! this.session ) {
 			// send heartbeat msg
-			var api = arguments.callee.name;
+			var api = 'heartbeat';
 			var data = { sid:this.session, t:Date.now() };
 			this.postMsg( { 
 				api: api, 
@@ -306,7 +306,7 @@ hotjs.inherit( User, AjaxClient, {
 		}
 		
 		// send logout msg
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'logout', {
 			sid : this.session
 		});
 		this.session = "";
@@ -320,13 +320,13 @@ hotjs.inherit( User, AjaxClient, {
 				newusername : newu,
 				newpwd : newpwd
 			};
-		var msg = this.callAPI( arguments.callee.name, param);
+		var msg = this.callAPI( 'changeIdPassword', param);
 		return (!! msg) ? msg.done : false;
 	},
 	
 	// { public: {}, protected: {}, private: {} }
 	updateProfile : function updateProfile( data ) {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'updateProfile', {
 			sid : this.session,
 			profile : data
 		} );
@@ -334,7 +334,7 @@ hotjs.inherit( User, AjaxClient, {
 	},
 	// get own profile: {}
 	getProfile : function getProfile() {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'getProfile', {
 			sid : this.session
 		} );
 		return ((!! msg) && msg.done) ? msg.profile : false;
@@ -343,7 +343,7 @@ hotjs.inherit( User, AjaxClient, {
 	// return: msg.profile as a list: [ {}, {}, {} ]
 	// only return public part & protected on situation, never return private profile
 	queryProfile : function queryProfile( user ) {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'queryProfile', {
 			sid : this.session,
 			user : user
 		} );
@@ -353,7 +353,7 @@ hotjs.inherit( User, AjaxClient, {
 	// query profile of others, may be many [ user1, user2, user3, ... ]
 	// return: msg.user as a list: [ { username:"user1", fullname: "David", presence: "online" }, ... ]
 	listFriend : function listFriend() {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'listFriend', {
 			sid : this.session
 		} );
 		if ( (!! msg) && msg.done && Array.isArray(msg.user) ) {
@@ -370,7 +370,7 @@ hotjs.inherit( User, AjaxClient, {
 		if( this.friends.indexOf(username) > -1 ) {
 			return false;
 		}
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'addFriend', {
 			sid : this.session,
 			username : username,
 			textmsg : textmsg
@@ -385,7 +385,7 @@ hotjs.inherit( User, AjaxClient, {
 		if( this.friends.indexOf(username) > -1 ) {
 			return false;
 		}
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'confirmAddFriend', {
 			sid : this.session,
 			username : username
 		});
@@ -399,7 +399,7 @@ hotjs.inherit( User, AjaxClient, {
 		if( this.friends.indexOf(username) == -1 ) {
 			return false;
 		}
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'removeFriend', {
 			sid : this.session,
 			username : username
 		});
@@ -416,7 +416,7 @@ hotjs.inherit( User, AjaxClient, {
 	// query profile of others, may be many [ user1, user2, user3, ... ]
 	// return: msg.user as a list: [ { username:"user1", fullname: "David", presence: "online" }, ... ]
 	listBlock : function listBlock() {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'listBlock', {
 			sid : this.session
 		} );
 		if ( (!! msg) && msg.done && Array.isArray(msg.user) ) {
@@ -434,7 +434,7 @@ hotjs.inherit( User, AjaxClient, {
 		if( this.friends.indexOf(u) > -1 ) {
 			return false;
 		}
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'block', {
 			sid : this.session,
 			username : u
 		});
@@ -448,7 +448,7 @@ hotjs.inherit( User, AjaxClient, {
 		if( this.friends.indexOf(u) == -1 ) {
 			return false;
 		}
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'unblock', {
 			sid : this.session,
 			username : u
 		});
@@ -464,7 +464,7 @@ hotjs.inherit( User, AjaxClient, {
 
 	// set tags, [ "hacker", "swimming", "golf", ... ]
 	setTag : function addTag( data ) {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'setTag', {
 			sid : this.session,
 			tag : data
 		} );
@@ -477,7 +477,7 @@ hotjs.inherit( User, AjaxClient, {
 		return false;
 	},
 	getTag : function getTag() {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'getTag', {
 			sid : this.session
 		} );
 		if( (!! msg) && msg.done ) {
@@ -491,7 +491,7 @@ hotjs.inherit( User, AjaxClient, {
 	},
 	// { presence: "busy", location: [lat, lang] }
 	updateStatus : function updateStatus( data ) {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'updateStatus', {
 			sid : this.session,
 			status : data
 		});
@@ -505,7 +505,7 @@ hotjs.inherit( User, AjaxClient, {
 	// query presence of others, may be many [ user1, user2, user3, ... ]
 	// return [ "available"/"busy"/"nodistrub"/"rightback"/"away"/"offline" ]
 	queryPresence : function queryPresence( users ) {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'queryPresence', {
 			sid : this.session,
 			user : users
 		} );
@@ -514,7 +514,7 @@ hotjs.inherit( User, AjaxClient, {
 	
 	// return: msg.user as a list: [ { username:"user1", fullname: "David", presence: "online" }, ... ]
 	searchUser : function searchUser( tag, distance ) {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'searchUser', {
 			sid : this.session,
 			tag : tag,
 			distance : distance
@@ -524,7 +524,7 @@ hotjs.inherit( User, AjaxClient, {
 	// filter is string with complex condition, like: "(age > 25) AND (age < 40) AND (tag CONTAIN 'reading')" :-)
 	// return: msg.user as a list: [ { username:"user1", fullname: "David", presence: "online" }, ... ]
 	searchUserComplex : function searchUserComplex( filter ) {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'searchUserComplex', {
 			sid : this.session,
 			filter : filter
 		} );
@@ -533,7 +533,7 @@ hotjs.inherit( User, AjaxClient, {
 	
 	// invite a friend to join a group
 	inviteJoinGroup : function inviteJoinGroup( name, g ) {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'inviteJoinGroup', {
 			sid : this.session,
 			name : name,
 			group : g
@@ -541,7 +541,7 @@ hotjs.inherit( User, AjaxClient, {
 		return ((!! msg) && msg.done) ? true : false;
 	},
 	quitGroup : function quitGroup( g ) {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'quitGroup', {
 			sid : this.session,
 			group : g
 		} );
@@ -549,7 +549,7 @@ hotjs.inherit( User, AjaxClient, {
 	},
 	// chat in a group
 	chat : function chat( s, g ) {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'chat', {
 			sid : this.session,
 			group : g,
 			what : s
@@ -559,7 +559,7 @@ hotjs.inherit( User, AjaxClient, {
 	
 	// return msg.room [ "room1", "room2", ... ], not all, limit to 20
 	findRoom : function findRoom( name ) {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'findRoom', {
 			sid : this.session,
 			name : name
 		} );
@@ -568,7 +568,7 @@ hotjs.inherit( User, AjaxClient, {
 	// if room not exist, then create it.
 	// may need enter password, if required
 	enterRoom : function enterRoom( name, pwd ) {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'enterRoom', {
 			sid : this.session,
 			name : name,
 			secret : pwd
@@ -577,7 +577,7 @@ hotjs.inherit( User, AjaxClient, {
 	},
 	// if no one in room after exist, then remove the password if there is.
 	leaveRoom : function leaveRoom( name ) {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'leaveRoom', {
 			sid : this.session,
 			name : name
 		} );
@@ -585,7 +585,7 @@ hotjs.inherit( User, AjaxClient, {
 	},
 	// say something, anyone in the room can hear.
 	say : function say( s ) {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'say', {
 			sid : this.session,
 			what : s
 		} );
@@ -594,7 +594,7 @@ hotjs.inherit( User, AjaxClient, {
 	
 	// send short message, only between friends.
 	sms : function sms( name, s ) {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'sms', {
 			sid : this.session,
 			name : name,
 			what : s
@@ -603,7 +603,7 @@ hotjs.inherit( User, AjaxClient, {
 	},
 	
 	searchGamer : function searchGamer( appkey, level ) {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'searchGamer', {
 			sid : this.session,
 			appkey : appkey,
 			level : level
@@ -613,7 +613,7 @@ hotjs.inherit( User, AjaxClient, {
 	},
 	
 	uploadGameData : function uploadGameData( appkey, md5key, result, steps ) {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'uploadGameData', {
 			sid : this.session,
 			appkey : appkey,
 			md5key : md5key,
@@ -626,7 +626,7 @@ hotjs.inherit( User, AjaxClient, {
 	listGameData : function listGameData( appkey, pagesize, pageindex ) {
 		if( pageindex === undefined ) pageindex = 0;
 		if( pagesize === undefined ) pagesize = 10;
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'listGameData', {
 			sid : this.session,
 			appkey : appkey,
 			pagesize : pagesize,
@@ -637,7 +637,7 @@ hotjs.inherit( User, AjaxClient, {
 	},
 	// msg = { "md5key" : "xx", "result" : "xx", "steps" : "xx" }
 	downloadGameData : function downloadGameData( appkey, md5key ) {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'downloadGameData', {
 			sid : this.session,
 			appkey : appkey,
 			md5key : md5key
@@ -646,7 +646,7 @@ hotjs.inherit( User, AjaxClient, {
 		return msg;
 	},
 	voteGameData : function voteGameData( appkey, md5key, rating ) {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'voteGameData', {
 			sid : this.session,
 			appkey : appkey,
 			md5key : md5key,
@@ -657,7 +657,7 @@ hotjs.inherit( User, AjaxClient, {
 	},
 	
 	updateGameScore : function updateGameScore( appkey, data1, data2, data3 ) {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'updateGameScore', {
 			sid : this.session,
 			appkey : appkey,
 			data1 : data1,
@@ -668,7 +668,7 @@ hotjs.inherit( User, AjaxClient, {
 	},
 	// { "data1" : [ { "name" : "tom", "score" : 10 }, { ... } ], "data2" : [], "data3" : [] }
 	getGameScoreTop10 : function getGameScoreTop10( appkey ) {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'getGameScoreTop10', {
 			sid : this.session,
 			appkey : appkey
 		});
@@ -676,7 +676,7 @@ hotjs.inherit( User, AjaxClient, {
 	},
 	
 	feedback : function feedback( appkey, txt ) {
-		var msg = this.callAPI( arguments.callee.name, {
+		var msg = this.callAPI( 'feedback', {
 			sid : this.session,
 			msg : txt
 		});
